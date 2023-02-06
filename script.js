6ae8ea8ba994e5ae598a8243e162c1ee70a56499
Sean Grant <pscgrant95@gmail.com>
2023-02-06 13:22:56 -0800

    fixed errors, createElement instead of getElement

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const container = document.getElementById("container");
 function makeRows(rows, cols) {
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
-    for (i = 0; i < (rows * cols); i++) {
-        let cell = document.getElementById("div");
-        cell.innerText = (i + 1);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+        cell.innerText = (c + 1);
         container.appendChild(cell).className = "grid-item";
     };
 };
